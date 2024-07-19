609e2dfe2d3ccfd89a8b5abd199ccb20adedfdb9
ZvRzyan18 <rhenzalfred06@gmail.com>
2024-07-19 23:00:54 +0800

    Create MatrixTranslation.cpp

---
+++ Samples/MatrixTranslation.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include "gpx_cpp/math/Matrix.hpp"
+#include "gpx_cpp/math/MathUtils.hpp"
+
+using namespace GPX;
+using namespace std;
+
+
+int main() {
+ 
+/*
+ translation 
+*/
+ 
+ Matrix3x3 m1;
+ 
+ //translate (3, 5)
+ Translate(m1, 3, 5);
+ Print(m1);
+ 
+ 
+ cout << endl << endl;
+ 
+/*
+ multiply translate
+*/
+ Matrix3x3 m2;
+ 
+ //translate (2, 3)
+ MultiplyTranslate(m2, m2, 2, 3);
+ Print(m2);
+
+/*
+  Matrix4x4 translation
+*/
+
+ Matrix4x4 m3;
+ 
+ //translate (2, 8, 7)
+ Translate(m3, 2, 8, 7);
+ //print matrix
+ Print(m3);
+ 
+ 
+ cout << endl << endl;
+ 
+/*
+ multiply translate
+*/
+ Matrix4x4 m4;
+ //translate (8, 7, 6)
+ MultiplyTranslate(m4, m4, 8, 7, 6);
+ Print(m4);
+ 
+ return 0;
+}
+
