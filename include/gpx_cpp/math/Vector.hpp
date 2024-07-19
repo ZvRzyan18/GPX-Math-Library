f82713f2a19aaec483bbd3c1bdfe32ca79a99336
ZvRzyan18 <rhenzalfred06@gmail.com>
2024-07-20 00:48:21 +0800

    Update Vector.hpp

--- include/gpx_cpp/math/Vector.hpp
+++ include/gpx_cpp/math/Vector.hpp
@@ -22,184 +22,184 @@ namespace GPX {
 /*
  Vector2 class
 */
-	class Vector2 {
-		public:
-		float X, Y;
+class Vector2 {
+ public:
+ float X, Y;
 		
-		Vector2();
-		Vector2(Vector3 vector);
-		Vector2(Vector4 vector);
-		Vector2(float x, float y);
-		Vector2(float* array);
+ Vector2();
+ Vector2(Vector3 vector);
+ Vector2(Vector4 vector);
+ Vector2(float x, float y);
+ Vector2(float* array);
 
 /*
  assign operator
 */
-		void operator=(Vector2 vector);
-		void operator=(Vector3 vector);
-		void operator=(Vector4 vector);
+ void operator=(Vector2 vector);
+ void operator=(Vector3 vector);
+ void operator=(Vector4 vector);
 		
 /*
  compare operator
 */
-		bool operator==(Vector2 vector);
-		bool operator!=(Vector2 vector);
+ bool operator==(Vector2 vector);
+ bool operator!=(Vector2 vector);
 		
 /*
  arithmetic operator
 */
-		Vector2 operator+(Vector2 a);
-		Vector2 operator-(Vector2 a);
-		Vector2 operator*(Vector2 a);
-		Vector2 operator/(Vector2 a);
+ Vector2 operator+(Vector2 a);
+ Vector2 operator-(Vector2 a);
+ Vector2 operator*(Vector2 a);
+ Vector2 operator/(Vector2 a);
 
-		Vector2 operator+(float a);
-		Vector2 operator-(float a);
-		Vector2 operator*(float a);
-		Vector2 operator/(float a);
+ Vector2 operator+(float a);
+ Vector2 operator-(float a);
+ Vector2 operator*(float a);
+ Vector2 operator/(float a);
 		
-		Vector2& operator+=(Vector2 a);
-		Vector2& operator-=(Vector2 a);
-		Vector2& operator*=(Vector2 a);
-		Vector2& operator/=(Vector2 a);
+ Vector2& operator+=(Vector2 a);
+ Vector2& operator-=(Vector2 a);
+ Vector2& operator*=(Vector2 a);
+ Vector2& operator/=(Vector2 a);
 		
-		Vector2& operator+=(float a);
-		Vector2& operator-=(float a);
-		Vector2& operator*=(float a);
-		Vector2& operator/=(float a);
+ Vector2& operator+=(float a);
+ Vector2& operator-=(float a);
+ Vector2& operator*=(float a);
+ Vector2& operator/=(float a);
 		
 /*
  element access function
 */
-		float& operator[](int index);
+ float& operator[](int index);
 		
-		int GetSize();
-		float* GetData();
-		float* begin();
-		float* end();
-	};
+ int GetSize();
+ float* GetData();
+ float* begin();
+ float* end();
+};
 
 /*
  Vector3 class
 */
-	class Vector3 {
-		public:
-		float X, Y, Z;
+class Vector3 {
+ public:
+  float X, Y, Z;
 		
-		Vector3();
-		Vector3(Vector2 vector);
-		Vector3(Vector4 vector);
-		Vector3(float x, float y, float z);
-		Vector3(float* array);
+ Vector3();
+ Vector3(Vector2 vector);
+ Vector3(Vector4 vector);
+ Vector3(float x, float y, float z);
+ Vector3(float* array);
 
 /*
  assign operator
 */
-	 void	operator=(Vector2 vector);
-	 void operator=(Vector3 vector);
-	 void operator=(Vector4 vector);
+  void operator=(Vector2 vector);
+  void operator=(Vector3 vector);
+  void operator=(Vector4 vector);
 	 
 /*
  comapare operator
 */
-		bool operator==(Vector3 vector);
-		bool operator!=(Vector3 vector);
+  bool operator==(Vector3 vector);
+  bool operator!=(Vector3 vector);
 
 /*
  arithmetic operator
 */
-		Vector3 operator+(Vector3 a);
-		Vector3 operator-(Vector3 a);
-		Vector3 operator*(Vector3 a);
-		Vector3 operator/(Vector3 a);
+  Vector3 operator+(Vector3 a);
+  Vector3 operator-(Vector3 a);
+  Vector3 operator*(Vector3 a);
+  Vector3 operator/(Vector3 a);
 
-		Vector3 operator+(float a);
-		Vector3 operator-(float a);
-		Vector3 operator*(float a);
-		Vector3 operator/(float a);
+  Vector3 operator+(float a);
+  Vector3 operator-(float a);
+  Vector3 operator*(float a);
+  Vector3 operator/(float a);
 		
-		Vector3& operator+=(Vector3 a);
-		Vector3& operator-=(Vector3 a);
-		Vector3& operator*=(Vector3 a);
-		Vector3& operator/=(Vector3 a);
+  Vector3& operator+=(Vector3 a);
+  Vector3& operator-=(Vector3 a);
+  Vector3& operator*=(Vector3 a);
+  Vector3& operator/=(Vector3 a);
 		
-		Vector3& operator+=(float a);
-		Vector3& operator-=(float a);
-		Vector3& operator*=(float a);
-		Vector3& operator/=(float a);
+  Vector3& operator+=(float a);
+  Vector3& operator-=(float a);
+  Vector3& operator*=(float a);
+  Vector3& operator/=(float a);
 
 /*
  element access function
 */
-		float& operator[](int index);
+  float& operator[](int index);
 		
-		int GetSize();
-		float* GetData();
-		float* begin();
-		float* end();
-	};
+  int GetSize();
+  float* GetData();
+  float* begin();
+  float* end();
+};
 
 
 
 /*
  Vector4 class
 */
-	class Vector4 {
-		public:
-		float X, Y, Z, W;
+class Vector4 {
+ public:
+  float X, Y, Z, W;
 		
-		Vector4();
-		Vector4(Vector2 vector);
-		Vector4(Vector3 vector);
-		Vector4(float x, float y, float z, float w);
-		Vector4(float* array);
+  Vector4();
+  Vector4(Vector2 vector);
+  Vector4(Vector3 vector);
+  Vector4(float x, float y, float z, float w);
+  Vector4(float* array);
 
 /*
  assign operator
 */
-	 void	operator=(Vector2 vector);
-	 void operator=(Vector3 vector);
-	 void operator=(Vector4 vector);
+  void operator=(Vector2 vector);
+  void operator=(Vector3 vector);
+  void operator=(Vector4 vector);
 	 
 /*
  compare operator
 */
-		bool operator==(Vector4 vector);
-		bool operator!=(Vector4 vector);
+  bool operator==(Vector4 vector);
+  bool operator!=(Vector4 vector);
 
 /*
  arithmetic operator
 */
-		Vector4 operator+(Vector4 a);
-		Vector4 operator-(Vector4 a);
-		Vector4 operator*(Vector4 a);
-		Vector4 operator/(Vector4 a);
+  Vector4 operator+(Vector4 a);
+  Vector4 operator-(Vector4 a);
+  Vector4 operator*(Vector4 a);
+  Vector4 operator/(Vector4 a);
 
-		Vector4 operator+(float a);
-		Vector4 operator-(float a);
-		Vector4 operator*(float a);
-		Vector4 operator/(float a);
+  Vector4 operator+(float a);
+  Vector4 operator-(float a);
+  Vector4 operator*(float a);
+  Vector4 operator/(float a);
 		
-		Vector4& operator+=(Vector4 a);
-		Vector4& operator-=(Vector4 a);
-		Vector4& operator*=(Vector4 a);
-		Vector4& operator/=(Vector4 a);
+  Vector4& operator+=(Vector4 a);
+  Vector4& operator-=(Vector4 a);
+  Vector4& operator*=(Vector4 a);
+  Vector4& operator/=(Vector4 a);
 		
-		Vector4& operator+=(float a);
-		Vector4& operator-=(float a);
-		Vector4& operator*=(float a);
-		Vector4& operator/=(float a);
+  Vector4& operator+=(float a);
+  Vector4& operator-=(float a);
+  Vector4& operator*=(float a);
+  Vector4& operator/=(float a);
 
 /*
   element access function
 */
   float& operator[](int index);
 		
-		int GetSize();
-		float* GetData();
-		float* begin();
-		float* end();
-	};
+  int GetSize();
+  float* GetData();
+  float* begin();
+  float* end();
+};
 
 
 /*
@@ -211,23 +211,23 @@ namespace GPX {
 /*
  vector dot product
 */
-	float Dot(Vector2 a, Vector2 b);
-	float Dot(Vector3 a, Vector3 b);
-	float Dot(Vector4 a, Vector4 b);
+float Dot(Vector2 a, Vector2 b);
+float Dot(Vector3 a, Vector3 b);
+float Dot(Vector4 a, Vector4 b);
 
 /*
  vector magnitude
 */
-	float Length(Vector2 a);
-	float Length(Vector3 a);
-	float Length(Vector4 a);
+float Length(Vector2 a);
+float Length(Vector3 a);
+float Length(Vector4 a);
 
 /*
  vector normalize
 */
-	void Normalize(Vector2& out, Vector2 a);
-	void Normalize(Vector3& out, Vector3 a);
-	void Normalize(Vector4& out, Vector4 a);
+void Normalize(Vector2& out, Vector2 a);
+void Normalize(Vector3& out, Vector3 a);
+void Normalize(Vector4& out, Vector4 a);
 
 /*
  vector return normalize 
