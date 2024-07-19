901ed0bd29f3b0bd1c9844a3a1900fa15a5ee8b6
ZvRzyan18 <rhenzalfred06@gmail.com>
2024-07-19 22:36:37 +0800

    Create Trigonometry.hpp

---
+++ include/gpx_cpp/math/Trigonometry.hpp
@@ -0,0 +1,31 @@
+/*
+* This file is part of GPX Project under BSD 3-Clause License
+* see LICENSE.txt
+*/
+
+/*
+ file name : Trigonometry.hpp
+*/
+
+#ifndef GPX_TRIGONOMETRY_HPP
+#define GPX_TRIGONOMETRY_HPP
+
+namespace GPX {
+/*
+ sine
+*/
+float Sin(float theta) noexcept;
+
+/*
+ cosine
+*/
+float Cos(float theta) noexcept;
+
+/*
+ tangent
+*/
+float Tan(float theta) noexcept;
+
+}
+
+#endif
