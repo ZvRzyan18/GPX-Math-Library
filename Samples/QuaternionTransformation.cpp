d57b42d7d66af558eae5e1a23c9d1dc9dcadc3c0
ZvRzyan18 <rhenzalfred06@gmail.com>
2024-07-20 15:35:03 +0800

    Update QuaternionTransformation.cpp

--- Samples/QuaternionTransformation.cpp
+++ Samples/QuaternionTransformation.cpp
@@ -1,3 +1,12 @@
+/*
+* This file is part of GPX Project under BSD 3-Clause License
+* see LICENSE.txt
+*/
+
+/*
+ file name : QuaternionTransformation.cpp
+*/
+
 #include <iostream>
 #include "gpx_cpp/math/MathUtils.hpp"
 #include "gpx_cpp/math/Quaternion.hpp"
